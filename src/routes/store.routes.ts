134b25883520e06c5656ce5ed65fcf0e77563ab1
Sofía Mercado Moreno <sofiamercadomoreno@Sofias-MacBook-Pro.local>
2026-04-14 10:08:22 -0600

    Add routes

---
+++ src/routes/store.routes.ts
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import { StoreController } from "../controllers/store.controller";
+import { ProductController } from "../controllers/product.controller";
+
+const router = Router();
+const storeController = new StoreController();
+const productController = new ProductController();
+
+// GET /api/stores
+router.get("/", storeController.getAll);
+
+// GET /api/stores/:id
+router.get("/:id", storeController.getById);
+
+// GET /api/stores/:storeId/products
+router.get("/:storeId/products", productController.getByStoreIdWithTags);
+
+export default router;
