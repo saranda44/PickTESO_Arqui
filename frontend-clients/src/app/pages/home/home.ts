5abc64c6e61d292121851f4b5fa8dd320e935c1a
Sofía Mercado Moreno <sofiamercadomoreno@Sofias-MacBook-Pro.local>
2026-04-23 14:10:21 -0600

    Small changes

--- frontend-clients/src/app/pages/home/home.ts
+++ frontend-clients/src/app/pages/home/home.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+interface Restaurant {
+  name: string;
+  cafeteria: string;
+  image: string;
+}
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './home.html',
-  styleUrl: './home.scss',
+  styleUrls: ['./home.scss'],
 })
-export class Home {}
+export class Home {
 
+  restaurants: Restaurant[] = [
+    {
+      name: 'Mar y Mesa',
+      cafeteria: 'Cafetería Central',
+      image: 'https://images.unsplash.com/photo-1604908176997-125f25cc6f3d'
+    },
+    {
+      name: 'Clementine & Rye',
+      cafeteria: 'Cafetería Central',
+      image: 'https://images.unsplash.com/photo-1565299624946-b28f40a0ae38'
+    },
+    {
+      name: 'Second Street Noddles',
+      cafeteria: 'Cafetería Pedro Arrupe',
+      image: 'https://images.unsplash.com/photo-1559847844-5315695dadae'
+    }
+  ];
+
+}
