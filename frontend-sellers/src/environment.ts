8a0e9a9f4f246e1f2d86f31d2d9395d8506798e4
Ozcar Clemente <163335710+ozcarclemente@users.noreply.github.com>
2026-04-26 15:50:09 -0600

    feat: deploy ci/cd config

--- frontend-sellers/src/environment.ts
+++ frontend-sellers/src/environment.ts
@@ -1,13 +1,13 @@
 // export const environment = {
 //     production: false,
-//     apiGatewayApiUrl: 'http://localhost:3000',
-//     sellersApiUrl: 'http://localhost:3000/sellers',
-//     ordersApiUrl: 'http://localhost:3000/orders',
-// }; 
+//     apiGatewayApiUrl: 'http://localhost:3000/api',
+//     sellersApiUrl: 'http://localhost:3000/api/sellers',
+//     ordersApiUrl: 'http://localhost:3000/api/orders',
+// };
 
 export const environment = {
     production: true,
-    apiGatewayApiUrl: 'https://mocking.so',
-    sellersApiUrl: 'https://mocking.so/sellers',
-    ordersApiUrl: 'https://mocking.so/orders',
+    apiGatewayApiUrl: 'https://mocking.so/api',
+    sellersApiUrl: 'https://mocking.so/api/sellers',
+    ordersApiUrl: 'https://mocking.so/api/orders',
 };
--- notification-service/src/index.ts
+++ notification-service/src/index.ts
@@ -1,4 +1,6 @@
-import 'dotenv/config';
+import dotenv from 'dotenv';
+dotenv.config();
+
 import app from './app';
 
 const PORT = process.env.PORT || 3003;
